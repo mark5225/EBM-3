d56119ac7b3ccad0f0dbc2687241b2d08715ff0f
mark5225 <marklandavazo@gmail.com>
2025-01-09 09:07:41 -0700

    Update class-post-types.php

--- Elite-Basketball-Manager/includes/class-post-types.php
+++ Elite-Basketball-Manager/includes/class-post-types.php
@@ -1,55 +1,76 @@
-<?php
+ <?php
 namespace EBM;
 
 class PostTypes {
-    private static $instance = null;
-
-    private function __construct() {
-        add_action('init', array($this, 'register_post_types'), 0);
-    }
-
-    public static function get_instance() {
-        if (null === self::$instance) {
-            self::$instance = new self();
-        }
-        return self::$instance;
-    }
-
     public function register_post_types() {
         $this->register_team_post_type();
         $this->register_player_post_type();
     }
 
     private function register_team_post_type() {
-        $args = array(
-            'labels' => array(
-                'name' => __('Teams', 'elite-basketball-manager'),
-                'singular_name' => __('Team', 'elite-basketball-manager'),
-                // Add other labels...
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'supports' => array('title', 'editor', 'thumbnail'),
-            'menu_icon' => 'dashicons-groups',
-            'rewrite' => array('slug' => 'teams')
-        );
+        $labels = [
+            'name'               => 'Teams',
+            'singular_name'      => 'Team',
+            'menu_name'          => 'Teams',
+            'add_new'           => 'Add New Team',
+            'add_new_item'      => 'Add New Team',
+            'edit_item'         => 'Edit Team',
+            'new_item'          => 'New Team',
+            'view_item'         => 'View Team',
+            'search_items'      => 'Search Teams',
+            'not_found'         => 'No teams found',
+            'not_found_in_trash'=> 'No teams found in trash'
+        ];
+
+        $args = [
+            'labels'              => $labels,
+            'public'              => true,
+            'publicly_queryable'  => true,
+            'show_ui'            => true,
+            'show_in_menu'       => false, // We'll add this under our custom menu
+            'query_var'          => true,
+            'rewrite'            => ['slug' => 'team'],
+            'capability_type'    => 'post',
+            'has_archive'        => true,
+            'hierarchical'       => false,
+            'menu_position'      => null,
+            'supports'           => ['title', 'editor', 'thumbnail'],
+            'menu_icon'          => 'dashicons-groups'
+        ];
 
         register_post_type('ebm_team', $args);
     }
 
     private function register_player_post_type() {
-        $args = array(
-            'labels' => array(
-                'name' => __('Players', 'elite-basketball-manager'),
-                'singular_name' => __('Player', 'elite-basketball-manager'),
-                // Add other labels...
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'supports' => array('title', 'editor', 'thumbnail'),
-            'menu_icon' => 'dashicons-businessman',
-            'rewrite' => array('slug' => 'players')
-        );
+        $labels = [
+            'name'               => 'Players',
+            'singular_name'      => 'Player',
+            'menu_name'          => 'Players',
+            'add_new'           => 'Add New Player',
+            'add_new_item'      => 'Add New Player',
+            'edit_item'         => 'Edit Player',
+            'new_item'          => 'New Player',
+            'view_item'         => 'View Player',
+            'search_items'      => 'Search Players',
+            'not_found'         => 'No players found',
+            'not_found_in_trash'=> 'No players found in trash'
+        ];
+
+        $args = [
+            'labels'              => $labels,
+            'public'              => true,
+            'publicly_queryable'  => true,
+            'show_ui'            => true,
+            'show_in_menu'       => false, // We'll add this under our custom menu
+            'query_var'          => true,
+            'rewrite'            => ['slug' => 'player'],
+            'capability_type'    => 'post',
+            'has_archive'        => true,
+            'hierarchical'       => false,
+            'menu_position'      => null,
+            'supports'           => ['title', 'editor', 'thumbnail'],
+            'menu_icon'          => 'dashicons-businessman'
+        ];
 
         register_post_type('ebm_player', $args);
     }
